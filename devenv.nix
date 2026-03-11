d66c8ead2e1edcc8591b4c495bcde1ff7396d7ab
eyelidlessness <gnosis@gmail.com>
2026-03-11 10:23:18 -0700

    chore(CI): use devenv
    - Updates CI workflow to use devenv
    - Updates devenv config itself to accommodate requisite Python 3.10

--- devenv.nix
+++ devenv.nix
@@ -15,4 +15,15 @@
 		};
 	};
 
+	# Per https://stackoverflow.com/a/74732671, ensuring Python 3.10 is available
+	# is intended to address failure of `yarn install --frozen-lockfile` (as seen
+	# in CI prior to
+	# https://github.com/eyelidlessness/eyelidlessness.github.io/commit/26a1651).
+	languages.python = {
+		# TODO: config.cloud.ci.github.enable(?)
+		# See: https://devenv.sh/cloud/#github-ci-integration
+		enable = true;
+		version = "3.10";
+		lsp.enable = false;
+	};
 }
